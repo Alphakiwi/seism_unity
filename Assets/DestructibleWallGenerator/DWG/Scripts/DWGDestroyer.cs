9dffee4dfb2eaac5fbffbbcbffb9a8c8f23bcbb2
Denis <deniscossu@outlook.fr>
2022-03-11 09:10:34 +0100

    first commit

---
+++ Assets/DestructibleWallGenerator/DWG/Scripts/DWGDestroyer.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DWGDestroyer : MonoBehaviour {
+
+	public float force;
+
+	void OnCollisionEnter(Collision col){
+			ExplodeForce();
+			Destroy(GetComponent<DWGDestroyer>());
+	}
+
+	// Explode force by radius only if a destructible tag is found
+	void ExplodeForce(){
+		Vector3 explodePos = transform.position;
+		Collider[] colliders = Physics.OverlapSphere(explodePos, force/50);
+		foreach (Collider hit in colliders){
+			if(hit.GetComponent<Collider>().tag == "Destructible"){
+				if(hit.GetComponent<Rigidbody>()){
+					hit.GetComponent<Rigidbody>().isKinematic = false;
+					hit.GetComponent<Rigidbody>().AddExplosionForce(force, explodePos, force/50);
+				}
+			}
+		}
+	}
+}
---
+++ Assets/DestructibleWallGenerator/Example/Scripts/ThrowObject.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ThrowObject : MonoBehaviour {
+	public float force = 300f;
+	private float update;
+	private float direction = 0f;
+
+
+	void OnGUI()
+	{
+		force = GUI.HorizontalSlider(new Rect(25, 25, 800, 200), force, 0.0F, 1200.0F);
+		if (GUI.Button(new Rect(600,250,180,120),"Lancer le séisme"))
+
+		 {
+			while (direction < 1f)
+			{
+				Throw(direction,0,1-direction);
+				Throw(-direction,0,1-direction);
+				Throw(direction,0,-(1-direction));
+				Throw(-direction,0,-(1-direction));
+				direction += 0.05f;
+			}
+			direction = 0;
+		}
+	}
+
+
+	void Update(){
+	}
+
+	// Create a sphere and throw it
+	void Throw(float x, float y, float z){
+		GameObject go = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+		go.name = "Onde";
+		go.transform.localScale = new Vector3(0.5f,0.5f,0.5f);
+		go.transform.position = transform.position + new Vector3(x,y,z);
+		go.GetComponent<Renderer>().enabled = false;
+		go.AddComponent<Rigidbody>();
+		go.GetComponent<Rigidbody>().AddForce(new Vector3(x,y,z)* 5*force);
+		go.AddComponent<DWGDestroyer>().force = force;
+		Destroy(go,500f);
+
+	}
+}
